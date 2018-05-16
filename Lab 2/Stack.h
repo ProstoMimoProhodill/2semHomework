6841fb5261b28bed206e8d8e41d35090c94bca57
Andrew Gilenko <keshe4k@yandex.ru>
2018-05-16 17:55:18 +0300

    new class - stack_complex

--- Lab 2/Stack.h
+++ Lab 2/Stack.h
@@ -1,19 +1,28 @@
 #ifndef STACK_H_
 #define STACK_H_
 
+#include <stdio.h>
 #include <iostream>
+#include <string>
+#include <math.h>
 using namespace std;
 
-template <typename Re_type, typename Im_type> struct Complex
+template <typename A, typename B> decltype(auto) sum(A a, B b){ return a+b; }
+template <typename A> decltype(auto) sqrt_f(A a){ return sqrt(a); }
+template <typename A> decltype(auto) squad_f(A a){ return pow(a, 2); }
+template <typename A> decltype(auto) add_f(A a){ return a+a; }
+
+template <typename Type> struct Complex
 {
-	Re_type Real;
-	Im_type Imagine;
+	Type Real;
+	Type Imagine;
 };
 
 template <class Type> class Stack
 {
+protected:
+    int size;
 private:
-	int size;
 	typedef struct Element
 	{
 		Type data;
@@ -23,119 +32,224 @@ private:
 public:
 	Stack() : head(NULL), size(0) {}
 	~Stack() { del(); }
+
     int getSize() { return size; }
-	bool push(Type data);
-	bool pop();
-    void output();
-	void del();
-	Type get();
-};
 
-template <class Type> bool Stack<Type>::push(Type data)
-{
-	Element *elem = new Element;
-	(head == NULL) ? (elem->next = NULL) : (elem->next = head);
-	elem->data = data;
-	head = elem;
-	size++;
-	return true;
-}
-
-template <class Type> bool Stack<Type>::pop()
-{
-	if (size == 0){
-        return false;
-    }else{
-        Element *elem = head;
-        head = head->next;
-        delete elem;
-        size--;
+	virtual bool push(Type data){
+        Element *elem = new Element;
+        (head == NULL) ? (elem->next = NULL) : (elem->next = head);
+        elem->data = data;
+        head = elem;
+        size++;
         return true;
     }
-}
 
-template <class Type> Type Stack<Type>::get()
+	virtual bool pop(){
+        if (size == 0){
+            return false;
+        }else{
+            Element *elem = head;
+            head = head->next;
+            delete elem;
+            size--;
+            return true;
+        }
+    }
+
+    virtual void output(){
+        if (head != NULL) {
+            Element *current = head;
+            cout<<current->data<<"\n";
+            while (current->next != NULL) {
+                current = current->next;
+                cout<<current->data<<"\n";
+            }
+        }else {
+            cout<<"Stack is empty...\n";
+        }
+        cout<<"\n";
+    }
+
+	virtual void del(){
+        Element *current = head;
+        Element *elem = head;
+        while (elem != NULL)
+        {
+            current = current->next;
+            delete elem;
+            elem = current;
+        }
+        head = NULL;
+        size = 0;
+    }
+
+	Type get(){
+        if (head != NULL) {
+            Element *elem = head;
+            return elem->data;
+        }else {
+            cout<<"Stack is empty...\n";
+            return 0;
+        }
+    }
+
+};
+
+template <class Type> class Stack_Complex : public Stack<Type>
 {
-	if (head != NULL) {
-		Element *elem = head;
-		return elem->data;
-	}else {
-		cout<<"Stack is empty...\n";
-		return 0;
-	}
-}
+private:
+    typedef struct Element_Complex
+    {
+        Type Re_data;
+        Type Im_data;
+        Element_Complex *next;
+    };
+    Element_Complex *head;
+public:
+    Stack_Complex <Type>() : Stack <Type>() {}
 
-template <class Type> void Stack<Type>::output()
-{
-	if (head != NULL) {
-		Element *current = head;
-		cout<<current->data<<"\n";
-		while (current->next != NULL) {
-			current = current->next;
-			cout<<current->data<<"\n";
-		}
-	}else {
-		cout<<"Stack is empty...\n";
-	}
-	cout<<"\n";
-}
+    bool push(struct Complex <Type> data){
+        Element_Complex *elem = new Element_Complex;
+        (head == NULL) ? (elem->next = NULL) : (elem->next = head);
+        elem->Re_data = data.Real;
+        elem->Im_data = data.Imagine;
+        head = elem;
+        this->size++;
+        return true;
+    }
 
-template <class Type> void Stack<Type>::del()
-{
-	Element *current = head;
-	Element *elem = head;
-	while (elem != NULL)
-	{
-		current = current->next;
-		delete elem;
-		elem = current;
-	}
-	head = NULL;
-	size = 0;
-}
+    bool pop() override {
+        if (this->size == 0){
+            return false;
+        }else{
+            Element_Complex *elem = head;
+            head = head->next;
+            delete elem;
+            this->size--;
+            return true;
+        }
+    }
 
+    void del() override {
+        Element_Complex *current = head;
+        Element_Complex *elem = head;
+        while (elem != NULL)
+        {
+            current = current->next;
+            delete elem;
+            elem = current;
+        }
+        head = NULL;
+        this->size = 0;
+    }
+
+    void output() override {
+        if (head != NULL) {
+            Element_Complex *current = head;
+            cout<<current->Re_data<<"+i("<<current->Im_data<<")\n";
+            while (current->next != NULL) {
+                current = current->next;
+                cout<<current->Re_data<<"+i("<<current->Im_data<<")\n";
+            }
+        }else {
+            cout<<"Stack is empty...\n";
+        }
+        cout<<"\n";
+    }
+
+    struct Complex <Type> get(){
+        struct Complex <Type> comp;
+        if (head != NULL) {
+            Element_Complex *elem = head;
+            comp.Real = elem->Re_data;
+            comp.Imagine = elem->Im_data;
+        }else {
+            cout<<"Stack is empty...\n";
+            comp.Real = 0;
+            comp.Imagine = 0;
+        }
+        return comp;
+    }
+};
 
 template <class Type> Stack<Type> sort(Stack<Type> s)
 {
-    Stack<Type> result;
-    int size = s.getSize();
-    Type *a = new Type[size];
-    for(int i=0;i<size;i++){
-        a[i] = s.get();
-        s.pop();
-    }
+    Stack<Type> result, tower;
+    int s_size = s.getSize();
+    int count = s_size;
+    while(count!=0){
+        if(count==s_size){
+            result.push(s.get());
+            s.pop();
+        }else{
+            if(s.get()<=result.get()){
+                result.push(s.get());
+                s.pop();
+            }else{
+                for(int i=0;i<result.getSize();i++){
+                    tower.push(result.get());
+                    result.pop();
+                }
 
-    for(int i=size-1;i>=0;i--){
-        for(int j=0;j<i;j++){
-            if(a[j]>a[j+1]){
-                Type tmp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = tmp;
+                result.push(s.get());
+                s.pop();
+
+                for(int i=0;i<tower.getSize();i++){
+                    result.push(tower.get());
+                    tower.pop();
+                }
             }
         }
-     }
-
-     for(int i=size-1;i>=0;i--){
-        result.push(a[i]);
-     }
-
-     delete [] a;
-     return result;
+        count--;
+    }
+    return result;
 }
 
-template <class Type> Stack<Type> map(Stack<Type> s)
+template <class Type, typename Func> Stack<Type> *map(Stack<Type> *stack, Func func)
 {
+    Stack<Type> *result = new Stack<Type>;
+    Stack<Type> *S = new Stack<Type>;
 
+    int stack_size = stack->getSize();
+    for(auto i=0;i<stack_size;i++){
+        S->push(func(stack->get()));
+        stack->pop();
+    }
+
+    for(auto i=0;i<stack_size;i++){
+        result->push(S->get());
+        S->pop();
+    }
+
+    delete S;
+    return result;
 }
 
-template <class Type> Stack<Type> where(Stack<Type> s)
+template <class Type, typename Func> Stack<Type> *where(Stack<Type> *stack, Func func)
 {
+    Stack<Type> *result = new Stack<Type>;
+    Stack<Type> *S = new Stack<Type>;
 
+    int stack_size = stack->getSize();
+    for(auto i=0;i<stack_size;i++){
+        if(func(stack->get())) S->push(func(stack->get()));
+        stack->pop();
+    }
+
+    int s_size = S->getSize();
+    for(auto i=0;i<s_size;i++){
+        result->push(S->get());
+        S->pop();
+    }
+
+    delete S;
+    return result;
 }
 
-template <class Type> Stack<Type> reduce(Stack<Type> s)
+template <class Type> Stack<Type> *reduce(Stack<Type> *stack)
 {
-
+    Stack<Type> *result = new Stack<Type>;
+    return result;
 }
 
 template <class Type> Stack<Type> merger(Stack<Type> s)
@@ -143,9 +257,17 @@ template <class Type> Stack<Type> merger(Stack<Type> s)
 
 }
 
-template <class Type> Stack<Type> extractionOfSubsequence(Stack<Type> s)
+template <class Type> Stack<Type> extractionOfSubsequence(Stack<Type> s, string index_str)
 {
+    Stack<Type> result;
+    for(int i=0;i<index_str.length();i++){
+        string number;
+        number+=index_str[i];
+        //if(index_str[i] == " "){
 
+        //}
+    }
+    return result;
 }
 
 template <class Type> Stack<Type> concatenation(Stack <Type> s1, Stack <Type> s2)
@@ -168,9 +290,9 @@ template <class Type> Stack<Type> concatenation(Stack <Type> s1, Stack <Type> s2
     for(int i=size_s1 + size_s2-1;i>=0;i--){
         result.push(a[i]);
     }
+    delete [] a;
     return result;
 }
-
 
 #endif
 
--- Lab 2/main.cpp
+++ Lab 2/main.cpp
@@ -1,25 +1,63 @@
 #include <iostream>
+#include <functional>
+#include <algorithm>
 #include "Stack.h"
 
 using namespace std;
 
+template <typename F>
+void map_little(F func)
+{
+    for(int i=0;i<10;i++){
+        cout<<func(i)<<"\n";
+    }
+}
+
 int main()
 {
-	Stack <float> stack1;
-	Stack <float> stack2;
+    /*
+    int arr[] {1,2,3,4,5,6,7,8,9,10};
+    auto sum = [](int x, int y){ return x+y; };
+    for_each(arr, arr+10 , [](int x){ cout<<x<<"  "; } );
 
-	for (int i = 0; i < 5; i++) {
-		stack1.push(i+0.1561);
+
+    function <int(int)> add_my = [](int x){ return x+100; };
+    map_little(add_my);
+
+    cout<<"sum:  "<<sum(2.5161, 3.156)<<"\n";
+    system("pause");
+
+    */
+
+
+	Stack <int> stack1;
+	Stack_Complex <int> stack3;
+	Stack <int> *p = &stack3;
+	Stack <float> stack2;
+	for (int i = 0; i < 20; i++) {
+        struct Complex<int> comp;
+        comp.Real = i;
+        comp.Imagine = i+100;
+		stack1.push(i);
 		stack2.push(i+0.9822);
+		stack3.push(comp);
 	}
 
+	cout<<stack3.getSize()<<"\n";
+
+	//stack3.del();
+	stack3.output();
+	stack3.pop();
+
+	//cout<<stack3.getSize();
+
+	cout<<"elem:  "<<(stack3.get()).Imagine<<"\n";
+
+	/*
 	stack1.output();
-	stack2.output();
-    //sort(stack1).output();
+	(*map(&stack1, sqrt_f)).output();
 
-    concatenation(stack1, stack2).output();
-
-
+	*/
 
     return 0;
 }
